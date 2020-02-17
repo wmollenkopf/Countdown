6c15b413336cba458ba566fa7bacda757ccc0a9c
wmollenkopf <biri@biri.me>
2020-02-17 15:11:04 -0500

    Added days and hours to the countdown layout.

--- src/components/CountdownTimer/CountdownTimer.js
+++ src/components/CountdownTimer/CountdownTimer.js
@@ -9,8 +9,10 @@ class CountdownTimer extends React.Component {
         this.state = {
             interval: null,
             countdownStarted: false,
+            daysRemaining: 0,
+            hoursRemaining: 0,
             minutesRemaining: 0,
-           secondsRemaining: 0
+            secondsRemaining: 0
         }
      }
      
@@ -24,11 +26,11 @@ class CountdownTimer extends React.Component {
         const updateTime = () => {
             let now = new Date().getTime();
             let distance = counter.endTime.getTime() - now;
-            // let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            // let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             let seconds = Math.floor((distance % (1000 * 60)) / 1000)-1;
-            return({distance: distance, minutes: minutes, seconds: seconds});
+            return({distance: distance, days: days, hours: hours, minutes: minutes, seconds: seconds});
         }
 
         const startCount = () => {
@@ -43,14 +45,14 @@ class CountdownTimer extends React.Component {
                 console.log(`Starting interval id:`,interval);
                 let displayDelay = 1;
                 let remainingTime = updateTime();
-                this.setState({interval: interval, countdownStarted: true, minutesRemaining: remainingTime.minutes, secondsRemaining: remainingTime.seconds+displayDelay});
+                this.setState({interval: interval, countdownStarted: true, daysRemaining: remainingTime.days, hoursRemaining: remainingTime.hours, minutesRemaining: remainingTime.minutes, secondsRemaining: remainingTime.seconds+displayDelay});
                 // console.log(`minutes`,`${minutes}`)
                 // console.log(`seconds`,`${seconds}`)
                 
                 if (remainingTime.distance < 0) {
                     let newInterval = clearInterval(interval);
                     console.log(`Stopping interval id:`,this.state.interval);
-                    this.setState({interval:newInterval,countdownStarted: false, minutesRemaining: 0, secondsRemaining: 0});
+                    this.setState({interval:newInterval,countdownStarted: false, daysRemaining: 0, hoursRemaining: 0, minutesRemaining: 0, secondsRemaining: 0});
                 }
 
             }, 100); // how fast it should update the interval
@@ -59,18 +61,35 @@ class CountdownTimer extends React.Component {
         const stopCount = () => {
             let newInterval = clearInterval(this.state.interval);
             console.log(`Stopping interval id:`,this.state.interval);
-            this.setState({interval:newInterval,countdownStarted: false, minutesRemaining: 0, secondsRemaining: 0});
+            this.setState({interval:newInterval,countdownStarted: false, daysRemaining: 0, hoursRemaining: 0, minutesRemaining: 0, secondsRemaining: 0});
         }
 
         return (
             <div>
             <div id="countdownText">
             
-            {this.state.countdownStarted ? (
-                <div>{this.state.minutesRemaining.toString().padStart(2, '0')}:{this.state.secondsRemaining.toString().padStart(2, '0')}</div>
-            ) : (
-                <div>{`10:00`}</div>
-            )}
+            
+                <div>
+                    <table className="countdownTable">
+                        <tbody>
+                            <tr className="countdownValues">
+                                <td>{this.state.daysRemaining.toString().padStart(2, '0')}</td>
+                                <td>{this.state.hoursRemaining.toString().padStart(2, '0')}</td>
+                                <td>{this.state.minutesRemaining.toString().padStart(2, '0')}</td>
+                                <td>{this.state.secondsRemaining.toString().padStart(2, '0')}</td>
+                            </tr>
+                            <tr className="countdownLabels">
+                                <td>Days</td>
+                                <td>Hours</td>
+                                <td>Minutes</td>
+                                <td>Seconds</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+                
+                {this.state.countdownStarted ? (<div></div>) :(<div></div>)}
 
             
                 
